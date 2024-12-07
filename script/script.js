822a927e948f761ff916c7bd7e0fd0fbc2cd559c
Hasstyk228 <radu0833@gmail.com>
2024-12-07 19:34:41 +0200

    2v SIte_Shop

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let oneInf = document.getElementById("one-inf").onclick = function (event) {
+    window.open("","_self").focus()
+}
+
+let twoInf = document.getElementById("two-inf").onclick = function (event) {
+    window.open("","_self").focus()
+}
+
+let threeInf = document.getElementById("three-inf").onclick = function (event) {
+    window.open("","_self").focus()
+}
