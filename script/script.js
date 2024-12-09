002eeff8e9bbe928db01a7bfedb9c5d5dc505ce0
Hasstyk228 <radu0833@gmail.com>
2024-12-09 19:36:17 +0200

    3v SIte_Shop

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
+let shop = document.getElementById("button-shop").onclick = function (event) {
+    window.open("http://127.0.0.1:5500/zota-web-training/main/otherPage/shop.html","_self").focus()
+}
+
+let acount = document.getElementById("button-account").onclick = function (event) {
+    window.open("http://127.0.0.1:5500/zota-web-training/main/otherPage/account.html","_self").focus()
+}
+
+let menu = document.getElementById("button-home").onclick = function (event) {
+    window.open("http://127.0.0.1:5500/zota-web-training/main/index.html","_self").focus()
+}
+
 let oneInf = document.getElementById("one-inf").onclick = function (event) {
-    window.open("","_self").focus()
+    window.open("http://127.0.0.1:5500/zota-web-training/main/otherPage/page_1.html","_self").focus()
 }
 
 let twoInf = document.getElementById("two-inf").onclick = function (event) {
-    window.open("","_self").focus()
+    window.open("http://127.0.0.1:5500/zota-web-training/main/otherPage/page_2.html","_self").focus()
 }
 
 let threeInf = document.getElementById("three-inf").onclick = function (event) {
-    window.open("","_self").focus()
+    window.open("http://127.0.0.1:5500/zota-web-training/main/otherPage/page_3.html","_self").focus()
 }
